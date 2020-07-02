7b7fb4a104f024a6c797f61bd76cdca9fa72aa92
Romildo Júnior <jrmmendes@outlook.com>
2020-07-01 21:29:55 -0300

    Add favicons and tag color picker

--- src/dev.to/utils.ts
+++ src/dev.to/utils.ts
@@ -16,11 +16,20 @@ const formatTimestamp = (s: string) => {
   return `${day} ${month}, ${year}`
 }
 
+const getColor = (tags: string[]): string => {
+  if (tags.includes('javascript')) return '#F7DF1E';
+  if (tags.includes('typescript')) return '#007ACC';
+  if (tags.includes('docker')) return '#2496ED';
+  if (tags.includes('jest')) return '#C21325';
+  if (tags.includes('aws')) return '#FF9900';
+  return 'white';
+}
+
 export const articleToBlogCard = (data: Article[]) => data.map(post => ({
   link: post.url,
   tags: post.tag_list.slice(0,2),
   briefing: post.description,
   title: post.title,
   timestamp: formatTimestamp(post.published_at),
-  color: 'white',
+  color: getColor(post.tag_list.slice(0,2)),
 }))
